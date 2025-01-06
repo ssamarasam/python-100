c80a87144abfe8c48868a1af442e67b61105aba5
ssamarasam <samcsegct@gmail.com>
2025-01-06 06:21:20 +0530

    fundamentals

---
+++ app.py
@@ -0,0 +1,35 @@
+import math
+print("Hello World!")
+x = 1
+course_name = "Python Programming"
+print(course_name[0:1])
+first = 'John'
+last = 'Smith'
+full_name = f"{first} {last} {2+2}"
+print(full_name)
+print(course_name.lower())
+print(course_name.upper())
+print(course_name.title())
+print(course_name.lstrip())
+print(course_name.find("Pro"))
+print(course_name.replace("P", "J"))
+print("Pro" in course_name)
+print("Hello" in course_name)
+
+# // represents integer output
+print(10 / 3)
+print(10 // 3)
+
+print(math.ceil(3.1))
+print(abs(-101))
+print(round(10.7))
+
+x = input("x: ")
+print(type(x))
+x = float(x) + 1
+print(x)
+
+# bool
+# str
+# int
+# float
