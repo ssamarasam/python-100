f499197919af3dc5268e18aa390e099448cd6bf0
ssamarasam <samcsegct@gmail.com>
2025-06-02 05:29:33 +0530

    moved base template to project directory, added boostrap classes added 404 exception added url parameters for individual movie

--- small-projects/vidly/movies/views.py
+++ small-projects/vidly/movies/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Movie
 
@@ -19,3 +19,11 @@ def index(request):
     # return HttpResponse(output)
 
     return render(request, 'movies/index.html', {"movies": movies})
+
+
+def detail(request, movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+        return render(request, 'movies/detail.html', {'movie': movie})
+    except Movie.DoesNotExist:
+        raise Http404
