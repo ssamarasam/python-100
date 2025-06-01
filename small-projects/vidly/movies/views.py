a55a1c63dfbac7f25f37d6db3190a14259b22a6d
ssamarasam <samcsegct@gmail.com>
2025-06-01 16:12:09 +0530

    added code to render records from database added template and boostraap

--- small-projects/vidly/movies/views.py
+++ small-projects/vidly/movies/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
+
+# pipenv install pylint-django
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    # Movie.objects.all()
+    # Movie.objects.filter(release_year=1988)
+    # Movie.objects.get(id=1)
+
+    movies = Movie.objects.all()
+
+    # output = ','.join([m.title for m in movies])
+    # return HttpResponse("Hello World!")  # map this to a url
+    # return HttpResponse(output)
+
+    return render(request, 'movies/index.html', {"movies": movies})
